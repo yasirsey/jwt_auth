b6618522d33f0fb6d228e950f7251de1caa57f68
Yasir Seyrek <yasirsey@gmail.com>
2021-10-06 16:40:31 +0300

    User routes created

--- routers/user.js
+++ routers/user.js
@@ -2,6 +2,9 @@ const router = require('express').Router()
 
 const userController = require('../controllers/user')
 
-router.get('/', userController.getAllUsers)
+router.get('/', require('../middlewares/auth'), userController.getAll)
+
+router.post('/register', userController.register)
+router.post('/login', userController.login)
 
 module.exports = router
